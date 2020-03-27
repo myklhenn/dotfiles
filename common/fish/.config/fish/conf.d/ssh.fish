bd2c1467abe7afdffc156e03041193c79963f22f
Michael Henning <mykl951@gmail.com>
2020-03-27 04:49:51 -0700

    2020-03-27 dotfiles update - link.sh now uses printf since echo is too inconsistent - switched from fisher to oh-my-fish package manager - updated various scripts to use fzy instead of fzf - added abbreviations for some custom ssh commands

---
+++ common/fish/.config/fish/conf.d/ssh.fish
@@ -0,0 +1,13 @@
+# fish shell abbreviations sourced for ssh
+
+if test (which ssh-add)
+  __fish_load_abbrs \
+    # alias   command
+    "sa       % ssh-add-key"
+end
+
+if test (which ssh-keygen)
+  __fish_load_abbrs \
+    # alias   command
+    "sk       % ssh-new-key"
+end
--- link.sh
+++ link.sh
@@ -4,54 +4,54 @@
 #
 # Created by Michael Henning <mykl951@gmail.com> -- https://github.com/myklhenn
 
-GC="\033[0;32m" # green color
-YC="\033[0;33m" # yellow color
-CC="\033[0;36m" # cyan color
-RC="\033[0;31m" # red color
-NC="\033[0m" # no color
+GC="\033[0;32m%s\033[0m\n" # green color
+YC="\033[0;33m%s\033[0m\n" # yellow color
+CC="\033[0;36m%s\033[0m\n" # cyan color
+RC="\033[0;31m%s\033[0m\n" # red color
+NC="\033[0m%s\n" # normal color
 
 check_command() {
 	# ($1: name of command)
 	if ! which $1 > /dev/null; then
-		echo $YC"Could not find \"$1\" command on your system."$NC
-		echo $YC"Please install before using this script."$NC
+		printf $YC "Could not find \"$1\" command on your system."
+		printf $YC "Please install before using this script."
 		exit 1
 	fi
 }
 add_platform () {
 	# ($1: name of platform's directory)
-	echo $CC"found platform \"$1\""$NC
+	printf $CC "found platform \"$1\""
 	PLATFORMS="$PLATFORMS $1"
 }
 create_dir () {
 	# ($1: description of directory, $2: path to directory)
-	echo $YC"creating directory for \"$1\""$NC
+	printf $YC "creating directory for \"$1\""
 	mkdir -pv $2
 	if [ $? -ne 0 ]; then
-		echo $RC"error creating directory for \"$1\""$NC
+		printf $RC "error creating directory for \"$1\""
 		ERRORS=1;
 	fi
 }
 link_package () {
-	# ($1: name of package's directory)
-	echo $CC"creating symlinks for \"$1\""$NC
-	stow --restow --verbose --ignore=".DS_Store" --target=$HOME $1
+	# ($1: name of platform, $2: name of package's directory)
+	printf $CC "creating symlinks for \"$1/$2\""
+	stow --restow --verbose --ignore=".DS_Store" --target=$HOME $2
 	if [ $? -ne 0 ]; then
-		echo $RC"error creating symlinks for \"$1\""$NC
+		printf $RC "error creating symlinks for \"$1/$2\""
 		ERRORS=1;
 	fi
 }
 check_errors () {
 	if [ "$ITERM_PREFS" = "1" ]; then
-		echo $YC"\nNOTE: iTerm overwrites links when saving preferences."$NC
-		echo $YC"As a workaround, configure iTerm to load preferences from:"$NC
-		echo $YC"\n$HOME/.config/iterm\n"$NC
+		printf $YC "NOTE: iTerm overwrites links when saving preferences."
+		printf $YC "As a workaround, configure iTerm to load preferences from:"
+		printf $YC "$HOME/.config/iterm"
 	fi
 	if [ "$ERRORS" = "1" ]; then
-		echo $RC"done, but with errors.\n"$NC
+		printf $RC "done, but with errors."
 		exit 1
 	else
-		echo $GC"done.\n"$NC
+		printf $GC "done."
 		exit 0
 	fi
 }
@@ -81,7 +81,7 @@ for PLATFORM in $PLATFORMS; do
 	cd "$DOTFILES_DIR"/"$PLATFORM"
 	for PKG_DIR in ./*; do
 		PKG_NAME=$(basename $PKG_DIR)
-		link_package $PKG_NAME
+		link_package $PLATFORM $PKG_NAME
 		if [ "$PKG_NAME" = "iterm" ]; then ITERM_PREFS=1; fi
 	done
 done
