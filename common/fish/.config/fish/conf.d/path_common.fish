e9273c614302a8b80d2de60da4916a40c6ca1c86
Michael Henning <mykl951@gmail.com>
2020-07-31 15:03:59 -0700

    2020-07-31 dotfiles update - split paths.fish into per-platform startup files - added rlsync script for manipulating Resilio Sync's history files - added "appify*" scripts for creating macOS (web) app launchers

---
+++ common/fish/.config/fish/conf.d/path_common.fish
@@ -0,0 +1,3 @@
+# PATH environment variable modifications sourced for all platforms
+
+__fish_add_to_path "$HOME/.local/bin"
---
+++ macos/fish/.config/fish/conf.d/path_macos.fish
@@ -0,0 +1,4 @@
+# PATH environment variable modifications sourced for macOS
+
+__fish_add_to_path "/Applications/Vivaldi.app/Contents/MacOS"
+__fish_add_to_path "/Applications/Vivaldi Snapshot.app/Contents/MacOS"
